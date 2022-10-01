d901c365db0152493e509fb94de66ee1f694a994
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2022-10-01 14:16:04 +0300

    CoroutineScope completed.

--- app/src/main/java/com/example/coroutinessample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinessample/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.coroutinessample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val answer = networkCall()
+
+            withContext(Dispatchers.Main) {
+                Log.v("Patika", answer)
+            }
+        }
+        loopCall()
+    }
+
+    /**
+     * Loop call
+     * Infinite Loop
+     */
+    fun loopCall() {
+        var counter = 0
+        while (true) {
+            println("Patika: $counter ")
+            counter++
+        }
+    }
+
+
+    /**
+     * Network call
+     *
+     * @return String
+     */
+    suspend fun networkCall(): String {
+        delay(2000L)
+        return "Network Called"
     }
 }
